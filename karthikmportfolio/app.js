f1f2bb6bcbdde2efd3dae1e2a440dbf7c00a2111
Karthik Meda <karthmeda@gmail.com>
2017-01-31 09:07:24 -0500

    Added stuff for portfolio

--- karthikmportfolio/app.js
+++ karthikmportfolio/app.js
@@ -6,7 +6,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+// var users = require('./routes/users');
+var about = require('./routes/about.js');
+
 
 var app = express();
 
@@ -29,7 +31,10 @@ app.use(require('node-sass-middleware')({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+// app.use('/users', users);
+app.use('/about',require('./routes/about'));
+app.use('/contact',require('./routes/contact'));
+app.use('/projects',require('./routes/projects'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
